b3c05fbdf7aabbbc1b5a099578d6a94cfac401cd
TechgicusSoftware <TechgicusAppwrite@gmail.com>
2024-12-30 16:03:30 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {  Router, Route, Routes } from "react-router-dom";
-import SplashScreen from "./Pages/SplashScreen";
+import {BrowserRouter as  Router, Route, Routes } from "react-router-dom";
+
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/home" element={<Home />} />
+       
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );

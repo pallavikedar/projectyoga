5205216c752ba5bdd47291a4c1eef10c8ca7cf6e
TechgicusSoftware <TechgicusAppwrite@gmail.com>
2025-01-01 14:31:41 +0530

    just

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {HashRouter as  Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as  Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
 
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
        
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}/>
       </Routes>
     </Router>
   );
